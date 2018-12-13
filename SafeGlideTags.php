3bfe7f47d26f08be37adc2319375e6f35b6aeb54
Danny Richardson <danny@pixelprimate.com>
2018-12-13 15:56:57 +0000

    first commit

---
+++ SafeGlideTags.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Statamic\Addons\SafeGlide;
+
+use Statamic\Addons\Glide\GlideTags;
+use Statamic\Extend\Tags;
+use Statamic\API\Asset;
+use Statamic\API\Config;
+
+class SafeGlideTags extends GlideTags
+{
+    public function __call( $method, $args )
+    {
+
+      // get parameters
+      $tag = explode( ':', $this->tag, 2 )[1];
+      $item = array_get( $this->context, $tag );
+
+      // get addon preferences
+      $maxW = $this->getConfigInt( 'max_width', 0 ) ;
+      $maxH = $this->getConfigInt( 'max_height', 0 );
+      $use_log = $this->getConfigBool( 'use_log', false );
+      $fallback = $this->getConfigBool( 'fallback', true );
+
+      // only carry out checks if the maximum width and height are set
+      if( $maxW > 0 && $maxH > 0 ) {
+
+        // get the dimensions of this asset
+        $asset = Asset::find( $item );
+        $w = $asset->width();
+        $h = $asset->height();
+
+        // if it's too large...
+        if ( $w > $maxW || $h > $maxH ) {
+
+          // log (if required)
+          if( $use_log ) {
+            \Log::info(
+              "Asset `$item` ($w x $h) is too large to resize. ".
+              "Maximum size is $maxW x $maxH. ".
+              ( $fallback
+                ? 'Falling back to original image.'
+                : 'The image was not served.'
+              )
+            );
+          }
+
+          // fall back to the original image (if required)
+          if( $fallback ) {
+            return $item; // the URL of the original image
+          } else {
+            return "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"; // 1x1 transparent GIF
+          }
+
+        }
+
+      } else {
+
+        \Log::info(
+          "SafeGlide has done nothing as the maximum width and height are not set. Update these in Settings > Addons > SafeGlide."
+        );
+
+      }
+
+      //pass everything to Glide
+      return parent::__call( $method, $args );
+
+    }
+}
---
+++ resources/lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'section_dimensions' => 'Dimensions',
+    'section_dimensions_instruct' => "The maximum width and height of images before SafeGlide kicks in",
+    'max_width' => 'Maximum width',
+    'max_height' => 'Maximum height',
+    'section_fallback' => "Fallback",
+    'section_fallback_instruct' => "If the image is too large to process – should SafeGlide fallback to outputting the original, unprocessed, image?",
+    'fallback' => 'Fallback to original image?',
+    'section_log' => 'Logging',
+    'section_log_instruct' => "SafeGlide can log occasions when an image is not processed by Glide because it is too large – useful for troubleshooting or debugging",
+    'use_log' => 'Use log?',
+];
